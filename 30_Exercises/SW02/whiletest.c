32a150dad8134ff026f2752f3a04f467894496b5
MuelKenHSLU <ken.mueller@stud.hslu.ch>
2020-09-23 11:01:23 +0200

    in class exercises

--- 30_Exercises/SW02/whiletest.c
+++ 30_Exercises/SW02/whiletest.c
@@ -2,13 +2,20 @@
 
 int main( int argc, char* argv[] )
 {
-	int limitValue = 5;
+	int limitValue = 200;
+	int maxLoopExecutions = 100;
 	int counter = 0;
 	while( counter < limitValue )
 	{
 		counter++; 
 		// ist das gleiche wie : counter = counter + 1
 		printf("# of while loop executions: %d\n", counter);
+		if( counter > maxLoopExecutions)
+		{	
+			printf("max #executions exceeded! Aborting. \n");
+			break;
+		}	
 	}
 	return 0;
 }
+
